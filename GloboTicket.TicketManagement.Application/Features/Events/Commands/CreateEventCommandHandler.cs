a43b648037e1e031e48e37aaf15740563dcf474f
PZiamanesh <po.ziamanesh@gmail.com>
2024-09-16 19:13:58 +0330

    add infrastructure

--- GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEventCommandHandler.cs
+++ GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEventCommandHandler.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using GloboTicket.TicketManagement.Application.Contracts.Exceptions;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
 using GloboTicket.TicketManagement.Application.Contracts.Persistence;
 using GloboTicket.TicketManagement.Application.Features.Events.Validators;
+using GloboTicket.TicketManagement.Application.Models.Mail;
 using GloboTicket.TicketManagement.Domain.Entities;
 using MediatR;
 
@@ -11,13 +13,16 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands
     {
         private readonly IAsyncRepository<Event> _eventRepository;
         private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
 
         public CreateEventCommandHandler(
             IAsyncRepository<Event> eventRepository,
-            IMapper mapper)
+            IMapper mapper,
+            IEmailService emailService)
         {
             _eventRepository = eventRepository;
             _mapper = mapper;
+            _emailService = emailService;
         }
 
         public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
@@ -32,6 +37,23 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands
 
             var @event = _mapper.Map<Event>(request);
             var createdEvent = await _eventRepository.AddAsync(@event);
+
+            try
+            {
+                var email = new Email()
+                {
+                    To = "po2.ziamanesh@gmail.com",
+                    Subject = "A new event was created!",
+                    Body = $"hi!\nevent {request.Name} created!\n{request.Description}"
+                };
+
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception)
+            {
+
+                // log and other procedures
+            }
 
             return createdEvent.EventId;
         }
---
+++ GloboTicket.TicketManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GloboTicket.TicketManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<GloboTicketDbContext>(ops =>
+            {
+                ops.UseSqlServer(configuration.GetConnectionString("GloboTicketConnString"));
+            });
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+
+            services.AddScoped<IEventRepository, EventRepository>();
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            return services;
+        }
+    }
+}
